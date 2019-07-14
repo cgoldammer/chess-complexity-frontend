3c0bb532ca903eda16a683694d92bde35651181a
cgoldammer <goldammer.christian@gmail.com>
2019-07-14 16:58:45 +0000

    More changes after linting

--- src/ChessApp.jsx
+++ src/ChessApp.jsx
@@ -3,29 +3,28 @@ import ReactTable from 'react-table';
 import styles from './ChessApp.css';
 import { Table, ToggleButtonGroup, ToggleButton, ButtonGroup, Panel, ListGroup, ListGroupItem, Navbar, Nav, NavItem, NavDropdown, Grid, Row, Col, Button, DropdownButton, MenuItem, FormControl, Breadcrumb, Modal } from 'react-bootstrap';
 import Chessdiagram from 'react-chessdiagram'; 
-import { defaultGetRows, calculateMoveNumber } from "./helpers.jsx";
+import { defaultGetRows } from './helpers.jsx';
 
-const lightSquareColor = '#f2f2f2'
-const darkSquareColor = '#bfbfbf'
-const currentPosition =  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'; // starting position
+const lightSquareColor = '#f2f2f2';
+const darkSquareColor = '#bfbfbf';
 const flip = false;
 
 const width = window.innerWidth;
 const maxSquareSize = 50;
-const squareSize = Math.min(width / 13, maxSquareSize)
+const squareSize = Math.min(width / 13, maxSquareSize);
 
 const cols = [
   {
-    Header: "Move",
-    accessor: "moveNumber"
+    Header: 'Move',
+    accessor: 'moveNumber'
   },
   {
-    Header: "White",
-    accessor: "white"
+    Header: 'White',
+    accessor: 'white'
   },
   {
-    Header: "Black",
-    accessor: "black"
+    Header: 'Black',
+    accessor: 'black'
   }
 ];
 const moveColumns = cols;
@@ -42,11 +41,11 @@ export class MoveTable extends React.Component {
     super(props);
   }
   getMoves = () => defaultGetRows(this.props.pgn);
-  rowMapper = (row, index) => ({moveNumber: row[0], white: row[1], black: row[2]});
+  rowMapper = row => ({moveNumber: row[0], white: row[1], black: row[2]});
   getData = () => this.getMoves().map(this.rowMapper);
   render = () => {
     var data = this.getData();
-    if (data.length == 0) return <div style={{textAlign: "center"}}>No moves yet</div>
-    return <div id="moveTable" className={styles.gameTable}> <ReactTable pageSize={data.length} showPagination={false} data={data} columns={moveColumns} getTdProps={this.onRowClick}/></div>
+    if (data.length == 0) return <div style={{textAlign: 'center'}}>No moves yet</div>;
+    return <div id='moveTable' className={styles.gameTable}> <ReactTable pageSize={data.length} showPagination={false} data={data} columns={moveColumns} getTdProps={this.onRowClick}/></div>;
   }
 }
--- src/helpers.jsx
+++ src/helpers.jsx
@@ -1,42 +1,28 @@
-import { List } from 'immutable';
-
-/* A hack for a weird problem: The import is handled differently
-when running in webpack-dev-server and through jest. 
-Just importing twice, and using the one version that works */
-import { Chess } from 'chess.js';
-import Chess2 from 'chess.js';
-
-import { getBest } from './engine.js';
 import * as tf from '@tensorflow/tfjs';
 
+const eloScale = 3000;
 
-
-//const tf = require("@tensorflow/tfjs");
-//import * as tfn from "@tensorflow/tfjs-node";
-//const model = tf.loadModel(handler);
-
-
-export const startingFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+export const startingFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 
 export const gameStatus = {
-  starting: [0, "New Game", "info"]
-, active: [1, "Active Game", "primary"]
-, whiteWon: [2, "White won", "danger"]
-, blackWon: [3, "Black won", "danger"]
-, draw: [4, "Draw", "warning"]
-}
+  starting: [0, 'New Game', 'info'],
+  active: [1, 'Active Game', 'primary'],
+  whiteWon: [2, 'White won', 'danger'],
+  blackWon: [3, 'Black won', 'danger'],
+  draw: [4, 'Draw', 'warning'],
+};
 
 export const invertLetters = fen => {
-	const tempLetter = 'T';
-	var fenInvert = fen;
-	for (var letter of 'pnrqk'){
-		const upper = letter.toUpperCase()
-		fenInvert = fenInvert.replace(new RegExp(letter, 'g'), tempLetter)
-		fenInvert = fenInvert.replace(new RegExp(upper, 'g'), letter)
-		fenInvert = fenInvert.replace(new RegExp(tempLetter, 'g'), upper)
-	}
-	return fenInvert;
-}
+  const tempLetter = 'T';
+  var fenInvert = fen;
+  for (var letter of 'pnrqk'){
+    const upper = letter.toUpperCase();
+    fenInvert = fenInvert.replace(new RegExp(letter, 'g'), tempLetter);
+    fenInvert = fenInvert.replace(new RegExp(upper, 'g'), letter);
+    fenInvert = fenInvert.replace(new RegExp(tempLetter, 'g'), upper);
+  }
+  return fenInvert;
+};
 
 export const invertFen = fen => {
 
@@ -44,52 +30,50 @@ export const invertFen = fen => {
   const positionPart = parts[0];
   const colorPart = parts[1];
 
-	if (colorPart == 'w') {
-		return fen;
-	}
+  if (colorPart == 'w') {
+    return fen;
+  }
 
   const castlePart = parts[2];
 
-	const fenInvertLetters = invertLetters(positionPart)
-	const fenSplit = fenInvertLetters.split('/');
+  const fenInvertLetters = invertLetters(positionPart);
+  const fenSplit = fenInvertLetters.split('/');
 
-	const positionPartInvert = fenSplit.reverse().join('/')
-	const castlePartInvert = invertLetters(castlePart);
+  const positionPartInvert = fenSplit.reverse().join('/');
+  const castlePartInvert = invertLetters(castlePart);
 
   return positionPartInvert + ' w ' + castlePartInvert + ' ' + parts.slice(3).join(' ');
-}
-
+};
 
 export const fillFen = fen => {
   var fenFill = fen;
   for (var i=1; i<=8; i++){
-    fenFill = fenFill.replace(new RegExp('' + i, 'g'), 'E'.repeat(i))
+    fenFill = fenFill.replace(new RegExp('' + i, 'g'), 'E'.repeat(i));
   }
-  return fenFill.replace(new RegExp('/', 'g'), '')
-}
+  return fenFill.replace(new RegExp('/', 'g'), '');
+};
 
 const allPieces = 'pPnNbBrRqQkK';
-const castlingValues = 'kKqQ'
+const castlingValues = 'kKqQ';
 
 export const getFeatures = fen => {
   const fenFilled = fillFen(fen);
-  var val;
-  var features = []
+  var features = [];
   for (var val of fenFilled){
     for (var piece of allPieces){
-      features.push(1.0 * (val == piece))
+      features.push(1.0 * (val == piece));
     }
   }
   return features;
-}
+};
 
 export const getCastleFeatures = castleString => {
-  var features = []
+  var features = [];
   for (var s of castlingValues){
-    features.push(castleString.includes(s) * 1.0)
+    features.push(castleString.includes(s) * 1.0);
   }
   return features;
-}
+};
 
 /* Getting all features used for the prediction model
  * In case `isWhite==True`, we invert the `fen`, because
@@ -97,7 +81,7 @@ export const getCastleFeatures = castleString => {
  */
 export const getAllFeatures = (fen, elo) => {
   if (fen == undefined) {
-    return []
+    return [];
   }
   const fenWhite = invertFen(fen);
 
@@ -109,16 +93,16 @@ export const getAllFeatures = (fen, elo) => {
   const featuresPosition = getFeatures(positionPart);
   const featuresCastle = getCastleFeatures(castlePart);
     
-  const featuresOther = [elo / 3000];
+  const featuresOther = [elo / eloScale];
 
-  return featuresPosition.concat(featuresCastle).concat(featuresOther)
-}
+  return featuresPosition.concat(featuresCastle).concat(featuresOther);
+};
 
 //export const getPrediction = features => 1;
 
 export async function getPrediction(features) {
   const model = await tf.loadLayersModel('https://chessinsights.org/analysis/sharpness_js/model.json');
-	const data = tf.tensor2d([features], [1, features.length]);
+  const data = tf.tensor2d([features], [1, features.length]);
   const pred = model.predict(data);
-	return pred
+  return pred;
 }
